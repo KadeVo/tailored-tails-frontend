c567dc0415965d2bbc55621c8b3ecd7c5b0f6824
Paul David Tan <cryxuz@gmail.com>
2023-11-17 11:19:30 +1300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import { HashRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
-import Home from './pages/Home';
-import Items from './pages/Items';
-import Cart from './pages/Cart';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import SingleItem from './pages/SingleItem';
-import NotFound from './components/NotFound';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Success from './pages/Success';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Items from './pages/Items'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer'
+import SingleItem from './pages/SingleItem'
+import NotFound from './components/NotFound'
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Success from './pages/Success'
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <ToastContainer />
-        <Navbar />
-        <Routes>
-          <Route path="/success" element={<Success />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/items/:itemId" element={<SingleItem />} />
-          <Route path="/registration" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <main className="container mx-auto p-2">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/success" element={<Success />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/items/:itemId" element={<SingleItem />} />
+        <Route path="/registration" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <main className="container mx-auto p-2">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
